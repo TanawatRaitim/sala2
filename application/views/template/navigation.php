871902b08c5a09abdea09f8dc1cc47b8b9700fea
Tanawat Raitim <tanardroid@gmail.com>
2014-02-28 18:41:34 +0700

    Navigation
    Still work in progress

--- application/views/template/navigation.php
+++ application/views/template/navigation.php
@@ -3,74 +3,11 @@
 			<a href="<?php echo base_url();?>" class="element"><span class="icon-heart"></span> ทะเบียนสมาชืกศาลาบริการ <sup>2.0</sup></a>
 			<span class="element-divider"></span>
 			<a class="pull-menu" href="#"></a>
+			
+			
+			<?php if($this->session->userdata['identity']=='administrator'):?>
+			
 			<ul class="element-menu">
-				<li>
-					<a class="dropdown-toggle" href="#">CRUD Examples</a>
-					<ul class="dropdown-menu dark" data-role="dropdown">
-						<li>
-							<a href='<?php echo site_url('main/customers_management')?>'>Customers</a>
-						</li>
-						<li>
-							<a href='<?php echo site_url('main/orders_management')?>'>Orders</a>
-						</li>
-						<li>
-							<a href='<?php echo site_url('main/products_management')?>'>Products</a>
-						</li>
-						<li>
-							<a href='<?php echo site_url('main/offices_management')?>'>Offices</a>
-						</li>
-						<li>
-							<a href='<?php echo site_url('main/employees_management')?>'>Employees</a>
-						</li>
-						<li>
-							<a href='<?php echo site_url('main/film_management')?>'>Films</a>
-						</li>
-						<li>
-							<a href='<?php echo site_url('main/film_management_twitter_bootstrap')?>'>Twitter Bootstrap Theme [BETA]</a>
-						</li>
-						<li>
-							<a href='<?php echo site_url('main/multigrids')?>'>Multigrid [BETA]</a>
-						</li>
-						<!-- <li>
-							<a href="#" class="dropdown-toggle">General CSS</a>
-							<ul class="dropdown-menu dark" data-role="dropdown">
-								<li>
-									<a href="global.html">Global styles</a>
-								</li>
-								<li>
-									<a href="grid.html">Grid system</a>
-								</li>
-								<div class="divider"></div>
-								<li>
-									<a href="typography.html">Typography</a>
-								</li>
-								<li>
-									<a href="tables.html">Tables</a>
-								</li>
-								<li>
-									<a href="forms.html">Forms</a>
-								</li>
-								<li>
-									<a href="buttons.html">Buttons</a>
-								</li>
-								<li>
-									<a href="images.html">Images</a>
-								</li>
-							</ul>
-						</li> -->
-						<li class="divider"></li>
-						<li>
-							<a href="responsive.html">Responsive</a>
-						</li>
-						<li class="disabled">
-							<a href="layouts.html">Layouts and templates</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a href="icons.html">Icons</a>
-						</li>
-					</ul>
-				</li>
 				<li>
 					<a class="dropdown-toggle"  href="#">Admin Menus</a>
 					<ul class="dropdown-menu dark" data-role="dropdown">
@@ -83,23 +20,11 @@
 						<li>
 							<a href='<?php echo site_url('main/pocketbooks')?>'>Pocket Books</a>
 						</li>
-						<!-- <li class="disabled">
-							<a href="http://blog.metroui.net">Blog</a>
-						</li>
-						<li class="disabled">
-							<a href="http://forum.metroui.net">Community Forum</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a href="https://github.com/olton/Metro-UI-CSS">Github</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a href="https://github.com/olton/Metro-UI-CSS/blob/master/LICENSE">License</a>
-						</li> -->
 					</ul>
 				</li>
-			</ul>
+			</ul> <!-- end element-menu -->
+			
+			<?php endif;?>
 
 			<div class="no-tablet-portrait">
 				<span class="element-divider"></span>
@@ -119,15 +44,6 @@
 				<div class="element place-right">
 					<a class="dropdown-toggle" href="#"> <span class="icon-cog"></span> </a>
 					<ul class="dropdown-menu place-right dark" data-role="dropdown">
-						<li>
-							<a href="#">Products</a>
-						</li>
-						<li>
-							<a href="#">Download</a>
-						</li>
-						<li>
-							<a href="#">Support</a>
-						</li>
 						<li>
 							<a href="<?php echo base_url().'auth/change_password';?>">เปลี่ยนรหัสผ่าน</a>
 						</li>
